e7576d707e49d6eadf4bb5a496aa7b87bc979096
Jatin kumar <65010397+jatin1234kumar@users.noreply.github.com>
2024-06-24 18:03:54 +0530

    created a responsive navbar for the level 1 website

--- LEVEL 1/TASK 2 - PORTFOLIO/script.js
+++ LEVEL 1/TASK 2 - PORTFOLIO/script.js
@@ -1,7 +1,42 @@
 let iconImg = document.getElementsByClassName('iconImg');
 
-Array.from(iconImg).forEach((element)=>{
-    element.parentElement.style.backgroundColor = element.parentElement.dataset.color;
+Array.from(iconImg).forEach((element) => {
+  element.parentElement.style.backgroundColor = element.parentElement.dataset.color;
+});
+
+
+// 
+
+const links = document.getElementById('myLinks');
+const navbar = document.getElementById('nav');
+const hamburger = document.getElementById('hamburger')
+
+let width = 950;
+let innerWidth = window.innerWidth;
+
+function addHamburger() {
+  links.classList.add('hidden');
+  hamburger.classList.remove('hidden');
+}
+
+function removeHamburger() {
+  links.classList.remove('hidden');
+  hamburger.classList.add('hidden');
+}
+
+if(innerWidth <= width) {
+  addHamburger();
+} else if (innerWidth > width){
+  removeHamburger();
+}
+
+
+window.addEventListener('resize', function () {
+  if (this.window.innerWidth <= width) {
+    addHamburger()
+  } else if ((this.window.innerWidth > width)) {
+    removeHamburger()
+  }
 });
 
 // hamburger responce
@@ -11,4 +46,9 @@ function myFunction(x) {
 }
 
 // responsive navbar
+
+hamburger.addEventListener('click', (e) => {
+  myFunction(e.target);
+  navbar.classList.toggle('responsive');  
+})
 
